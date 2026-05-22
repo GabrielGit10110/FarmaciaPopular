10b043cf4d8b9e497e5221ce2ffbcb8fe61c9edd
Roukan <gabrieloliveira13945@gmail.com>
2026-05-22 10:54:00 -0300

    feat: added entity logic
    - added atributes
    - added getters
    - added setters

--- app/src/main/java/edu/curso/model/Medicamento.java
+++ app/src/main/java/edu/curso/model/Medicamento.java
@@ -1,5 +1,56 @@
 package edu.curso.model;
 
-public class Medicamento {
+import java.time.LocalDate;
 
+public class Medicamento {
+    private long id;
+    private String nome;
+    private String codBarras;
+    private LocalDate dataEntrega;
+    private LocalDate dataVencimento;
+    private boolean farmPopular;
+    private double valor;
+
+    public Medicamento() {
+        this.id = 0;
+        this.nome = "";
+        this.codBarras = "";
+        this.dataEntrega = LocalDate.now();
+        this.dataVencimento = LocalDate.now();
+        this.farmPopular = false;
+        this.valor = 0.0;
+    }
+
+    public Medicamento(long id, String nome, String codBarras, LocalDate dataEntrega,
+        LocalDate dataVencimento, boolean farmPopular, double valor
+    ) {
+        this.id = id;
+        this.nome = nome;
+        this.codBarras = codBarras;
+        this.dataEntrega = dataEntrega;
+        this.dataVencimento = dataVencimento;
+        this.farmPopular = farmPopular;
+        this.valor = valor;
+    }
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCodBarras() { return codBarras; }
+    public void setCodBarras(String codBarras) { this.codBarras = codBarras; }
+
+    public LocalDate getDataEntrega() { return dataEntrega; }
+    public void setDataEntrega(LocalDate dataEntrega) { this.dataEntrega = dataEntrega; }
+
+    public LocalDate getDataVencimento() { return dataVencimento; }
+    public void setDataVencimento(LocalDate dataVencimento) { this.dataVencimento = dataVencimento; }
+
+    public boolean isFarmPopular() { return farmPopular; }
+    public void setFarmPopular(boolean farmPopular) { this.farmPopular = farmPopular; }
+
+    public double getValor() { return valor; }
+    public void setValor(double valor) { this.valor = valor; }
 }
