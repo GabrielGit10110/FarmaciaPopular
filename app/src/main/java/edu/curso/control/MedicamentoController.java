8b3e9c65c8287fc1e2c715b0b4be3dac5eb68432
Roukan <gabrieloliveira13945@gmail.com>
2026-05-22 13:18:20 -0300

    feat: added Controller logic
    - added save() feature
    - added update() feature
    - added delete() feature
    - added atributes with Property
    - added Medicamento ObservableList for UI
    - added prints for debugging in Controller
    - added prints for debugging in DAO Implementation

--- app/src/main/java/edu/curso/control/MedicamentoController.java
+++ app/src/main/java/edu/curso/control/MedicamentoController.java
@@ -4,20 +4,85 @@ import java.util.List;
 
 import edu.curso.infraestructure.MedicamentoImplMariaDB;
 import edu.curso.model.Medicamento;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.beans.property.*;
+import java.time.LocalDate;
+
 
 public class MedicamentoController {
-    private final MedicamentoDAO dao = new MedicamentoImplMariaDB();
+    private final MedicamentoDAO dao;
+
+    private ObservableList<Medicamento> lista = FXCollections.observableArrayList();
+
+    private LongProperty id = new SimpleLongProperty(0);
+    private StringProperty nome = new SimpleStringProperty("");
+    private StringProperty codBarras = new SimpleStringProperty("");
+    private ObjectProperty<LocalDate> dataEntrega = new SimpleObjectProperty(LocalDate.now());
+    private ObjectProperty<LocalDate> dataVencimento = new SimpleObjectProperty(LocalDate.now());
+    private BooleanProperty farmPopular = new SimpleBooleanProperty(false);
+    private DoubleProperty valor = new SimpleDoubleProperty(0.0);
+
+    public MedicamentoController(MedicamentoDao dao) {
+        this.dao = dao;
+        load();
+    }
+
+    public void clearFields() {
+        this.id.set(0);
+        this.nome.set("");
+        this.codBarras.set("");
+        this.dataEntrega.set(LocalDate.now());
+        this.dataVencimento.set(LocalDate.now());
+        this.farmPopular.set(false);
+        this.valor.set(0);
+    }
+
+    public void fromEntity(Medicamento m) {
+        if (m != null) {
+            this.id.set( m.getId() );
+            this.nome.set( m.getNome() );
+            this.codBarras.set( m.getCodBarras() );
+            this.dataEntrega.set( m.getDataEntrega() );
+            this.dataVencimento.set( m.getDataVencimento() );
+            this.farmPopular.set( m.isFarmPopular() );
+            this.valor.set( m.getValor() );
+        }
+    }
+
+    public Medicamento toEntity() {
+        Medicamento m = new Medicamento(
+                                    this.id.get(),
+                                    this.nome.get(),
+                                    this.codBarras.get(),
+                                    this.dataEntrega.get(),
+                                    this.dataVencimento.get(),
+                                    this.farmPopular.get(),
+                                    this.valor.get()
+                                );
+        return m;
+    }
 
     public void save() {
+        Medicamento m = toEntity();
 
+        if (this.id.get() > 0) {
+            System.out.println("Atualizando Medicamento...\n" + m.toString());
+            this.dao.update(id.get(), m);
+        } else {
+            System.out.println("Salvando Novo Medicamento...\n" + m.toString());
+            this.dao.save(m);
+        }
+
+        clearFields();
+        load();
     }
 
-    public void delete() {
-
-    }
-
-    public void update() {
-
+    public void delete(int indice) {
+        Medicamento m = this.lista.get(indice);
+        System.out.println("Deletando medicamento...\n" + m.toString());
+        this.dao.delete(m);
+        load();
     }
 
     public Medicamento searchById(long id) {
@@ -32,4 +97,33 @@ public class MedicamentoController {
         return this.dao.searchByCode(codBarras);
     }
 
+    public void load() {
+        this.lista.clear();
+        this.lista.addAll(
+            this.dao.findAll()
+        );
+    }
+
+    public long getId() { return this.id.get(); }
+    public LongProperty idProperty() { return this.id; }
+
+    public String getNome() { return this.nome.get(); }
+    public StringProperty nomeProperty() { return this.nome; }
+
+    public String getCodBarras() { return this.codBarras.get(); }
+    public StringProperty codBarrasProperty() { return this.codBarras; }
+
+    public LocalDate getDataEntrega() { return this.dataEntrega.get(); }
+    public ObjectProperty<LocalDate> dataEntregaProperty() { return this.dataEntrega; }
+
+    public LocalDate getDataVencimento() { return this.dataVencimento.get(); }
+    public ObjectProperty<LocalDate> dataVencimentoProperty() { return this.dataVencimento; }
+
+    public boolean isFarmPopular() { return this.farmPopular.get(); }
+    public BooleanProperty farmPopularProperty() { return this.farmPopular; }
+
+    public double getValor() { return this.valor.get(); }
+    public DoubleProperty valorProperty() { return this.valor; }
+
+    public ObservableList<Medicamento> getLista() { return this.lista; }
 }
--- app/src/main/java/edu/curso/infraestructure/MedicamentoImplMariaDB.java
+++ app/src/main/java/edu/curso/infraestructure/MedicamentoImplMariaDB.java
@@ -9,44 +9,37 @@ public class MedicamentoImplMariaDB implements MedicamentoDAO {
 
     @Override
     public List<Medicamento> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        System.out.println("Buscando todos os itens no banco de dados...");
     }
 
     @Override
     public void save(Medicamento m) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        System.out.println("Salvando medicamento no banco...");
     }
 
     @Override
     public void delete(Medicamento m) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        System.out.println("Deletando medicamento do banco...");
     }
 
     @Override
     public void update(long id, Medicamento m) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        System.out.println("Atualizando medicamento no banco...");
     }
 
     @Override
     public Medicamento searchById(long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'searchById'");
+        System.out.println("Buscando medicamento pelo id no banco...");
     }
 
     @Override
     public List<Medicamento> searchByName(String nome) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'searchByName'");
+        System.out.println("Buscando medicamentos pelo nome no banco...");
     }
 
     @Override
     public Medicamento searchByCode(String codBarras) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'searchByCode'");
+        System.out.println("Bsucando medicamentos pelo codigo no banco...");
     }
 
 }
