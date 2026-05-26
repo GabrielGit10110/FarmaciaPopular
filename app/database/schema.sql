30367d2938cfcaefc384dbe729f128f99326ac64
Roukan <gabrieloliveira13945@gmail.com>
2026-05-26 12:39:06 -0300

    Revert "Merge pull request #42 from GabrielGit10110/feature/medicamentos"
    This reverts commit 39e1169eb28043028b39af807f7836db2a4b8f95, reversing
    changes made to c379f4804b4ea28871457511dc407c65cf44a722.

---
+++ app/database/schema.sql
@@ -0,0 +1,9 @@
+
+CREATE DATABASE IF NOT EXISTS farmacia;
+USE farmacia;
+
+-- importar tabelas
+SOURCE medicamento.sql;
+SOURCE fornecedor.sql;
+SOURCE funcionario.sql;
+SOURCE cliente.sql;
---
+++ app/src/main/java/edu/curso/control/FornecedorController.java
@@ -0,0 +1,6 @@
+package edu.curso.control;
+
+public class FornecedorController {
+
+}
+//
