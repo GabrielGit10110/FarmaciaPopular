6643c63226c486a8ab3914fbb8bb9ba03272978e
Roukan <gabrieloliveira13945@gmail.com>
2026-05-26 10:48:06 -0300

    feat: added DAO and DAO In Memory Impl

--- app/src/main/java/edu/curso/control/FornecedorDAO.java
+++ app/src/main/java/edu/curso/control/FornecedorDAO.java
@@ -1,5 +1,16 @@
 package edu.curso.control;
 
-public class FornecedorDAO {
+import java.util.List;
+
+import edu.curso.model.Fornecedor;
+
+public interface FornecedorDAO {
+    public void save(Fornecedor f);
+    public void update(long id, Fornecedor f);
+    public void delete(Fornecedor f);
+    public List<Fornecedor> findAll();
+    public Fornecedor findById(long id);
+    public List<Fornecedor> findByName(String nome);
+    public Fornecedor findByCnpj(String cnpj);
 
 }
