9b406e3c9417db46de258ef4f913ca6237b9216c
Roukan <gabrieloliveira13945@gmail.com>
2026-05-26 15:08:44 -0300

    feat: added main Entity
    - feat: added Fornecedor entity

--- app/src/main/java/edu/curso/model/Fornecedor.java
+++ app/src/main/java/edu/curso/model/Fornecedor.java
@@ -1,6 +1,48 @@
 package edu.curso.model;
 
 public class Fornecedor {
+    private long id;
+    private String nome;
+    private String cnpj;
+    private String telefone;
+    private String email;
 
+    public Fornecedor() {
+        this.id = 0;
+        this.nome = "";
+        this.cnpj = "";
+        this.telefone = "";
+        this.email = "";
+    }
+
+    public Fornecedor(long id, String nome, String cnpj, String telefone, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Fornecedor(String nome, String cnpj, String telefone, String email) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCnpj() { return cnpj; }
+    public void setCnpj(String cnpj) { this.cnpj = cnpj; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
 //
