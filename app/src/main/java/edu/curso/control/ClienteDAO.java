1a63e356363a35e4eba0a586c50539e873e1b2f6
Gabriel Ordonho <gabriel.sordonho@gmail.com>
2026-06-09 12:01:23 -0300

    adicionando DAO e conexão com o banco de dados

--- app/src/main/java/edu/curso/control/ClienteDAO.java
+++ app/src/main/java/edu/curso/control/ClienteDAO.java
@@ -1,5 +1,17 @@
 package edu.curso.control;
 
+import java.util.List;
+
+import edu.curso.model.Cliente;
+
 public interface ClienteDAO {
+
+    public void save(Cliente c);
+    public void update(long id, Cliente c);
+    public void delete(Cliente c);
+    public List<Cliente> findAll();
+    public Cliente searchById(long id);
+    public List<Cliente> searchByNome(String nome);
+    public Cliente searchByCpf(String cpf);
 
 }
